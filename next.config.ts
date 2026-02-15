cf9256aabe3fed96e1f9e59d334db48e63b6da1c
christojin <superdev731@gmail.com>
2026-02-15 15:37:18 +0000

    fix: prisma engine binary

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  outputFileTracingIncludes: {
+    "/api/**": ["./src/generated/prisma/**/*"],
+    "/auth/**": ["./src/generated/prisma/**/*"],
+  },
   images: {
     remotePatterns: [
       {
